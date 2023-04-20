bf935335f5cc35e61383eb703eb59180ba2b3655
NickJ202 <nickjuliano20@gmail.com>
2023-04-20 19:26:21 -0400

    Add arweave request logging

--- src/clients/arweave/ArweaveClient.ts
+++ src/clients/arweave/ArweaveClient.ts
@@ -9,24 +9,48 @@ const POST_ENDPOINT = 'arweave.net';
 const PORT = 443;
 const PROTOCOL = 'https';
 const TIMEOUT = 40000;
-const LOGGING = false;
+const LOGGING = true;
 
 export default class ArweaveClient {
-	arweaveGet: any = Arweave.init({
-		host: GET_ENDPOINT,
-		port: PORT,
-		protocol: PROTOCOL,
-		timeout: TIMEOUT,
-		logging: LOGGING,
-	});
+  arweaveGet: any;
+  arweavePost: any;
+  warp: any;
 
-	arweavePost: any = Arweave.init({
-		host: POST_ENDPOINT,
-		port: PORT,
-		protocol: PROTOCOL,
-		timeout: TIMEOUT,
-		logging: LOGGING,
-	});
+  constructor() {
+    this.arweaveGet = Arweave.init({
+      host: GET_ENDPOINT,
+      port: PORT,
+      protocol: PROTOCOL,
+      timeout: TIMEOUT,
+      logging: LOGGING,
+    });
 
-	warp = WarpFactory.forMainnet({ ...defaultCacheOptions, inMemory: true });
+    this.arweavePost = Arweave.init({
+      host: POST_ENDPOINT,
+      port: PORT,
+      protocol: PROTOCOL,
+      timeout: TIMEOUT,
+      logging: LOGGING,
+    });
+
+    this.arweaveGet.request = this.wrapRequestForLogging(this.arweaveGet.request);
+    this.arweavePost.request = this.wrapRequestForLogging(this.arweavePost.request);
+
+    this.warp = WarpFactory.forMainnet({ ...defaultCacheOptions, inMemory: true });
+  }
+
+  wrapRequestForLogging(originalRequest) {
+    return async (method, url, headers, body, ...args) => {
+      console.log(`Arweave request: ${method} ${url}`);
+      console.log('Request headers:', headers);
+      console.log('Request body:', body);
+
+      const response = await originalRequest.call(this.arweaveGet, method, url, headers, body, ...args);
+
+      console.log('Response status:', response.status);
+      console.log('Response headers:', response.headers);
+
+      return response;
+    };
+  }
 }
